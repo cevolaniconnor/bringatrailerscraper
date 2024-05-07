06e728ab7d6bb1f20ff6c1b7348a04ac451a077c
cevolaniconnor <70539453+cevolaniconnor@users.noreply.github.com>
2024-05-07 16:47:38 -0700

    Added full functionality
    Added full functionality to the web scrapper. Scrapes key data including price, mileage, etc. from over 75 cars. All written to a json file that is keyed by the VIN and date of sale.

---
+++ project4.py
@@ -0,0 +1,113 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+import json
+
+driver = webdriver.Chrome()
+URL = "https://bringatrailer.com/porsche/991-gt2/?exclude=parts+wheels"
+
+resp = driver.get(URL)
+
+sleep(3)
+
+auctionLinks = []
+auctionDataComplete = {}
+
+showMoreButton = driver.find_elements(By.XPATH, '//button[@class = "button button-show-more"]')
+
+pageNumber = 1
+
+while True:
+	try:
+		showMoreButton = driver.find_elements(By.XPATH, '//button[@class="button button-show-more"]')
+		if showMoreButton:
+			showMoreButton[0].click()
+			print(f"Showing expansion page {pageNumber}")
+			sleep(3)
+			pageNumber += 1
+		else:
+			print("Show More button not found. Reached the last page.")
+			break
+
+	except:
+		break
+
+
+sleep(3)
+
+auction = driver.find_elements(By.XPATH, '//a[@class="listing-card bg-white-transparent"]')
+print(f"Scraping complete, {len(auction)} GT2 RS's processed")
+
+for link in auction:
+	href = link.get_attribute("href")
+	auctionLinks.append(href)
+
+for auction in auctionLinks:
+	auctionData = {}
+	driver.get(auction)
+
+	sleep(2)
+
+	VIN_elements = driver.find_elements(By.XPATH, '//a[contains(@href, "search?q=")]')
+	for vin in VIN_elements:
+		VINtext = vin.text.strip()
+		VIN = VINtext
+		
+
+	modelName = driver.find_elements(By.XPATH, '//h1[@class="post-title listing-post-title"]')
+	for title in modelName:
+		textTitle = title.text
+
+		auctionData['modelName'] = textTitle
+
+	auctionDetails = driver.find_elements(By.XPATH, '//span[@class="info-value noborder-tiny"]')
+	for elem in auctionDetails:
+		elemText	= elem.text.split()
+		price		= elemText[2].strip('$').replace(',', '')
+		date		= elemText[4]
+		result		= elemText[0]
+
+		auctionData['price']	= price
+		auctionData['date']		= date
+		auctionData['result']	= result
+
+	auctionTag = driver.find_elements(By.XPATH, '//div[@class = "item-tags"]')
+	auctionData['tags'] = ["No Tags"]  
+	if auctionTag:
+		auctionData['tags'] = []  
+		for tag in auctionTag:
+			textTag = tag.text.strip()
+			if textTag:
+				auctionData['tags'].append(textTag)  
+
+	sellerName = driver.find_elements(By.XPATH, '//div[@class="item item-seller"]')
+	for name in sellerName:
+		cleanedName = name.text.split()
+		auctionData['seller name'] = cleanedName[1]
+		
+
+	sellerLoc = driver.find_elements(By.XPATH, '//a[contains(@href, "maps/place")]')
+	for location in sellerLoc:
+		loc = location.text.strip()
+		auctionData['location'] = loc
+		
+
+	listingDetails = driver.find_elements(By.XPATH, '//div[@class = "item"]//ul//li')
+	details = []
+	for detail in listingDetails:
+		detailInfo = detail.text
+		details.append(detailInfo)
+
+		auctionData['listing details'] = details
+
+
+	key = str((VIN, date)) 
+	auctionDataComplete[key] = auctionData
+
+results = {"911 GT2 RS's": auctionDataComplete}
+
+with open("products.json", "w+") as f:
+	json.dump(results, f, indent = 4)
+f.close()
+
+driver.quit()
